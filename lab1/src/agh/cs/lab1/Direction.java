c60928198dd5ec8772b059f0d5f2e2a95a180d4a
MikolajKardys <66805019+MikolajKardys@users.noreply.github.com>
2020-10-16 16:52:39 +0200

    Commit_two_new_beginning
    Start od zera; Usuwamy plik testowy "Ziemniaczek.txt" i statrujemy od zera żeby wyeliminować chaos

---
+++ lab1/src/agh/cs/lab1/Direction.java
@@ -0,0 +1,10 @@
+package agh.cs.lab1;
+enum Dir {
+    FORWARD,
+    BACKWARD,
+    RIGHT,
+    LEFT
+}
+public class Direction {
+    
+}
--- lab1/src/agh/cs/lab1/World.java
+++ lab1/src/agh/cs/lab1/World.java
@@ -1,25 +1,8 @@
 package agh.cs.lab1;
 
 public class World {
-    public static void run(String dir[]){
-        int i = 0;
-        for (; i < dir.length - 1; i += 1){
-            switch(dir[i]){
-                case "f":
-                    System.out.println("Zwierzak idzie do przodu");
-                    break;
-                case "b":
-                    System.out.println("Zwierzak idzie do tyłu");
-                    break;
-                case "r":
-                    System.out.println("Zwierzak skręca w prawo");
-                    break;
-                case "l":
-                    System.out.println("Zwierzak skręca w lewo");
-                    break;
-            }
-        }
-        switch(dir[i]){
+    public static void print_comm(String c){
+        switch(c){
             case "f":
                 System.out.println("Zwierzak idzie do przodu");
                 break;
@@ -34,10 +17,15 @@ public class World {
                 break;
         }
     }
-    public static void main(String[] args) {
-        System.out.print("Start\n");
-        String dir [] = {"f", "f", "r", "l"};
-        run(dir);
-        System.out.print("Stop");
+    public static void run(String [] tab){
+        for (String arg : tab){
+            print_comm(arg);
+        }
+    }
+    public static void main(String [] args) {
+        System.out.println("Start Systemu");
+        run(args);
+        System.out.println("Stop Systemu");
     }
 }
+
