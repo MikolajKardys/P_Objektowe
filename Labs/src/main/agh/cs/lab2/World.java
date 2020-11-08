a5881b48a7cfbe2aa0493c286e1d565befec9ded
your_username <66805019+MikolajKardys@users.noreply.github.com>
2020-11-08 17:41:57 +0100

    Lab4 - ostateczne - v2
    Naprawione World.java

--- Labs/src/main/agh/cs/lab2/World.java
+++ Labs/src/main/agh/cs/lab2/World.java
@@ -2,9 +2,9 @@ package agh.cs.lab2;
 
 public class World {
     public static void main (String [] args){
-        IWorldMap map = new RectangularMap(3, 3);                                    //Mapa
-        MoveDirection[] directions = new OptionsParser().parse(new String [] {"f", "b", "r", "r", "f", "f", "f", "f", "f", "f", "r", "l", "f", "f"});//Argumenty
-        Vector2d [] positions = {new Vector2d(0,0), new Vector2d(0,3) };                //Zwierzęta
+        MoveDirection[] directions = new OptionsParser().parse(args);
+        IWorldMap map = new RectangularMap(10, 5);
+        Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
         IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
     }
